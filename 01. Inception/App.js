e3f92db38ec1c6dd7484278a602e7f0db432e15b
tripathi-g <shantanutripathi96@gmail.com>
2023-06-18 14:00:00 +0530

    nesting with React.createElement

--- 01. Inception/App.js
+++ 01. Inception/App.js
@@ -1,5 +1,19 @@
-const parent = React.createElement("div",{id: "parent"},React.createElement("div",{id: "child"},[React.createElement("h1",{},"I am an H1 Tag"),React.createElement("h2",{},"I am an H2 Tag")]))
+const parent = React.createElement(
+  "div",
+  { id: "parent" },[React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an H1 Tag"),
+    React.createElement("h2", {}, "I am an H2 Tag"),
+  ]),React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an H1 Tag"),
+    React.createElement("h2", {}, "I am an H2 Tag"),
+  ])]
+  
+);
 
-const heading = React.createElement("h1", {class: "heading    "}, "Hello World from React");
+const heading = React.createElement(
+  "h1",
+  { class: "heading    " },
+  "Hello World from React"
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
