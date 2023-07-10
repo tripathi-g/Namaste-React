0da4d0044216396948ba47739ba4ef52d02bf3d6
Shantanu Tripathi <55848734+tripathi-g@users.noreply.github.com>
2023-07-10 13:08:25 +0000

    Create 7 files

---
+++ 03 Laying the Foundation/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },[React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an H1 Tag"),
+    React.createElement("h2", {}, "I am an H2 Tag"),
+  ]),React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an H1 Tag"),
+    React.createElement("h2", {}, "I am an H2 Tag"),
+  ])]
+  
+);
+
+const heading = React.createElement(
+  "h1",
+  { class: "heading    " },
+  "Hello World from React"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+setTimeout(()=>{
+  root.render(parent);
+},2000)
